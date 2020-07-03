a6f9e2fa4bb1c677573a0db13eb2b02fba668915
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-07-03 17:56:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const Service = require('./service.js')
 const OS = require('os');
 const LibUSB = require('usb');
 
-a = new Service();
-//a.on('open', () => {
-// this.isStart = true
-//
-// this.log('Service Started')
-//})
+
 try {
- a.printer.print("tes");
+  service = new Service()
+  service.on('open', () => {
+    isStart = true
+    console.log('Service Started')
+  })
+  service.on('close', () => {
+    isStart = false
+    service = null
+    console.log('Service Stoped')
+  })
+  service.on('message', () => {
+    console.log('Receive printing message')
+  })
+  service.on('error', (e) => {
+    console.log(e)
+  })
+  service.start().catch((e) => {
+    console.log(e)
+  })
 } catch (e) {
- console.log(e)
+  console.log(e)
 }
-
-
